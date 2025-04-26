60e662f059ceac550d11fe0bc0aa8d2ef73b219e
SatyamPathak12 <satyampathak08122000k@gmail.com>
2025-04-27 01:27:47 +0530

    changes commit

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,7 +22,7 @@ function About() {
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Know Who <strong className="purple">I'M</strong>
+               <strong className="purple">Know Who I'M</strong>
             </h1>
             <Aboutcard />
           </Col>
@@ -35,7 +35,7 @@ function About() {
           </Col>
         </Row>
         <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
+          <strong className="purple">Professional Skillset </strong>
         </h1>
 
         <Techstack />
--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,26 +8,15 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Satyam Pathak </span>
-            from <span className="purple"> Chandauli, India.</span>
+            Hello Everyone, My name Satyam Pathak 
+            and i am from Varanasi Uttar Pradesh India.
             <br />
-            I am currently pursuing my B.Tech in Electronics and Communication Engineering at Madan Mohan Malaviya University of Technology
-            Gorakhpur UttarPradesh India.
+            <br/>
+            I am currently pursuing my Bachelor's degree in Electronics and Communication Engineering from Madan Mohan Malaviya University of Technology, Gorakhpur.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
           </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Chess
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writing Poetry
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
+          
         </blockquote>
       </Card.Body>
     </Card>
